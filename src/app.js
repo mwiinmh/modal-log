6bb389b6c136d65b528773fa3ba691daf2b019ce
mwiinmhe <mwindjoummhoumadi78@hotmail.com>
2025-02-10 18:06:59 +0100

    js

--- src/app.js
+++ src/app.js
@@ -13,14 +13,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     /* object js */
 
-    const modale = {
-        elementParent: document.querySelector(".modal-parent"),
-        btn_open: document.querySelector("h1, a"),
-        btn_closed: document.querySelector(".btn-closed"),
-        closed: document.querySelector(".closed"),
-        elementFigure: document.querySelector("figure")
-    }
-
     // Ouvrir la modale
     modale.btn_open.addEventListener("click", (e) => {
         e.stopPropagation();
